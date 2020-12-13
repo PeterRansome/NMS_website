2e5df6fb5cc03f762889a15aaa4e1132d5ad0216
NMSorg <75486051+NMSorg@users.noreply.github.com>
2020-12-13 23:35:16 +0000

    RL > R6

--- teams.php
+++ teams.php
@@ -32,7 +32,7 @@
                 </div>
           </div>
           <div class="col-sm-6">
-                <div class="portfolio-item"><a href="/rocket-league"><img src="/assets/img/rocket_team.jpg" alt="">
+                <div class="portfolio-item"><a href="/rainbow-six-siege"><img src="/assets/img/r6_team.jpg" alt="">
                 </a>
                 </div>
           </div>
