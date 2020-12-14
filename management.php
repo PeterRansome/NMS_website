1c8cf158917b62e000b1051002f752bb1f2dfc56
NMSorg <75486051+NMSorg@users.noreply.github.com>
2020-12-14 00:35:11 +0000

    lol

--- management.php
+++ management.php
@@ -83,7 +83,7 @@
 	            <h6>Community Manager</h6>
             </div>
             <div class="col-md-2 col-sm-6">
-	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+	            <p><img class="img-responsive center-block" src="/assets/img/tyler.jpg" alt=""></p>
 	            <h2 class="classic">NMS Tyler</h2>
 	            <ul class="list-inline">
 	              <li><a href="https://twitter.com/Tylerfifa23"><i class="fab fa-twitter fa-lg"></i></a></li>
