b7b81f261e3b6a273fb39330df9ac44ebc0336de
NMSorg <75486051+NMSorg@users.noreply.github.com>
2020-12-14 00:32:42 +0000

    Managers update and added

--- management.php
+++ management.php
@@ -11,10 +11,10 @@
       </div>
 	</header>  
 
-<section class="bg-gray text-center">
+	<section class="bg-gray text-center">
       <div class="container wow fadeIn">
         <div class="row justify-content-center">
-		<div class="col-md-2 col-sm-6">
+	          <div class="col-md-2 col-sm-6">
 	            <p><img class="img-responsive center-block" src="/assets/img/Ellis.jpg" alt=""></p>
 	            <h2 class="classic">NMS Ellis</h2>
 	            <ul class="list-inline">
@@ -54,11 +54,7 @@
 	            </ul>
 	            <h6>Media Manager</h6>
             </div>
-	        
-
-	    <div class="row justify-content-center">
-        
-	          <div class="col-md-2 col-sm-6">
+            <div class="col-md-2 col-sm-6">
 	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
 	            <h2 class="classic">NMS GeekDesgins</h2>
 	            <ul class="list-inline">
@@ -66,16 +62,35 @@
 	            </ul>
 	            <h6>Design Manager</h6>
 	          </div>
-            <div class="col-md-2 col-sm-6">
+	        
+
+	    <div class="row justify-content-center">
+        
+	          <div class="col-md-2 col-sm-6">
 	            <p><img class="img-responsive center-block" src="/assets/img/envy.jpg" alt=""></p>
 	            <h2 class="classic">NMS Envyy</h2>
 	            <ul class="list-inline">
 	              <li><a href="https://twitter.com/EnvyyGGs"><i class="fab fa-twitter fa-lg"></i></a></li>
 	            </ul>
 	            <h6>Fortnite Manager</h6>
-	          </div>     
-
-      </div>
+            </div>
+            <div class="col-md-2 col-sm-6">
+	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+	            <h2 class="classic">NMS Benny</h2>
+	            <ul class="list-inline">
+	              <li><a href="https://twitter.com/BennyB_UK"><i class="fab fa-twitter fa-lg"></i></a></li>
+	            </ul>
+	            <h6>Community Manager</h6>
+            </div>
+            <div class="col-md-2 col-sm-6">
+	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+	            <h2 class="classic">NMS Tyler</h2>
+	            <ul class="list-inline">
+	              <li><a href="https://twitter.com/Tylerfifa23"><i class="fab fa-twitter fa-lg"></i></a></li>
+	            </ul>
+	            <h6>Fifa Coach</h6>
+			  </div>
+		</div>
     </section>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/foot.inc.php"); ?>
