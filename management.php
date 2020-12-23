24606487e4c1473c1931ee2ad5a2e4d57a0e76e3
NMSorg <75486051+NMSorg@users.noreply.github.com>
2020-12-23 03:03:22 +0000

    should be done

--- management.php
+++ management.php
@@ -66,21 +66,21 @@
 
 	    <div class="row justify-content-center">
         
-	          <div class="col-md-2 col-sm-6">
-	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
-	            <h2 class="classic">NMS Kelly</h2>
-	            <ul class="list-inline">
-	              <li><a href="https://twitter.com/Kellyleo3"><i class="fab fa-twitter fa-lg"></i></a></li>
-	            </ul>
-	            <h6>Fifa Manager</h6>
-            </div>
-            <div class="col-md-2 col-sm-6">
+		<div class="col-md-2 col-sm-6">
 	            <p><img class="img-responsive center-block" src="/assets/img/benny.jpg" alt=""></p>
 	            <h2 class="classic">NMS Benny</h2>
 	            <ul class="list-inline">
 	              <li><a href="https://twitter.com/BennyB_UK"><i class="fab fa-twitter fa-lg"></i></a></li>
 	            </ul>
 	            <h6>Community Manager</h6>
+            </div>
+            <div class="col-md-2 col-sm-6">
+	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+	            <h2 class="classic">NMS Kelly</h2>
+	            <ul class="list-inline">
+	              <li><a href="https://twitter.com/Kellyleo3"><i class="fab fa-twitter fa-lg"></i></a></li>
+	            </ul>
+	            <h6>Fifa Manager</h6>
             </div>
             <div class="col-md-2 col-sm-6">
 	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
