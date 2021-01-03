a19c51dbfc1eb261289e4ec5ffb598559a2969aa
NMSorg <75486051+NMSorg@users.noreply.github.com>
2021-01-03 17:01:23 +0000

    Done
    Everything

--- content-creators.php
+++ content-creators.php
@@ -118,6 +118,14 @@
             </ul>
             <h6>Content Creator</h6>
           </div>
+          <div class="col-md-2 col-sm-6">
+            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+            <h2 class="classic">NMS Zlatxn</h2>
+            <ul class="list-inline">
+              <li><a href="https://twitter.com/Zlatxn11"><i class="fab fa-twitter fa-lg"></i></a></li>
+            </ul>
+            <h6>Content Creator</h6>
+          </div>
         </div>
         </div>
       </div>
