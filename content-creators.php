1feab9e75c8eb084d9d5cd66feed9d01c79310d6
NMSorg <75486051+NMSorg@users.noreply.github.com>
2021-01-17 00:20:14 +0000

    Changed the fifa and cc

--- content-creators.php
+++ content-creators.php
@@ -126,6 +126,14 @@
             </ul>
             <h6>Content Creator</h6>
           </div>
+          <div class="col-md-2 col-sm-6">
+            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+            <h2 class="classic">NMS Synergy</h2>
+            <ul class="list-inline">
+              <li><a href="https://twitter.com/SynergyFUT"><i class="fab fa-twitter fa-lg"></i></a></li>
+            </ul>
+            <h6>Content Creator</h6>
+          </div>
         </div>
         </div>
       </div>
