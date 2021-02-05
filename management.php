3e308455211589c5f795edff3dbc4bdc63eddd94
NMSorg <75486051+NMSorg@users.noreply.github.com>
2021-02-05 21:31:11 +0000

    Managments

--- management.php
+++ management.php
@@ -31,20 +31,20 @@
 	            <h6>Founder</h6>
             </div>
             <div class="col-md-2 col-sm-6">
-	            <p><img class="img-responsive center-block" src="/assets/img/poizah.png" alt=""></p>
-	            <h2 class="classic">NMS Poizah</h2>
+	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+	            <h2 class="classic">NMS Kelly</h2>
 	            <ul class="list-inline">
-	              <li><a href="https://twitter.com/Poizah_"><i class="fab fa-twitter fa-lg"></i></a></li>
+	              <li><a href="https://twitter.com/Kellyleo3"><i class="fab fa-twitter fa-lg"></i></a></li>
 	            </ul>
-	            <h6>Co Owner</h6>
-	          </div>
+	            <h6>Fifa Coach</h6>
+            </div>
             <div class="col-md-2 col-sm-6">
-	            <p><img class="img-responsive center-block" src="/assets/img/Noberrrrr.jpg" alt=""></p>
-	            <h2 class="classic">NMS Noberrrrr</h2>
+	            <p><img class="img-responsive center-block" src="/assets/img/benny.jpg" alt=""></p>
+	            <h2 class="classic">NMS Benny</h2>
 	            <ul class="list-inline">
-	              <li><a href="https://twitter.com/Noberrrrr"><i class="fab fa-twitter fa-lg"></i></a></li>
+	              <li><a href="https://twitter.com/BennyB_UK"><i class="fab fa-twitter fa-lg"></i></a></li>
 	            </ul>
-	            <h6>Co Owner</h6>
+	            <h6>Community Manager</h6>
             </div>
             <div class="col-md-2 col-sm-6">
 	            <p><img class="img-responsive center-block" src="/assets/img/Katie.jpg" alt=""></p>
@@ -62,6 +62,34 @@
 	            </ul>
 	            <h6>Fifa Coach</h6>
 	          </div>
+	        
+
+	    <div class="row justify-content-center">
+        
+            <div class="col-md-2 col-sm-6">
+	            <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+	            <h2 class="classic">NMS GeekDesgins</h2>
+	            <ul class="list-inline">
+	              <li><a href="https://twitter.com/GeekDesignGFX"><i class="fab fa-twitter fa-lg"></i></a></li>
+	            </ul>
+	            <h6>Design Manager</h6>
+            </div>
+            <div class="col-md-2 col-sm-6">
+              <p><img class="img-responsive center-block" src="/assets/img/jordanwheels.jpg" alt=""></p>
+              <h2 class="classic">NMS Jordanwheels</h2>
+              <ul class="list-inline">
+                <li><a href="https://twitter.com/jordangotwheels"><i class="fab fa-twitter fa-lg"></i></a></li>
+              </ul>
+              <h6>Content Manager US</h6>
+            </div>
+            <div class="col-md-2 col-sm-6">
+              <p><img class="img-responsive center-block" src="/assets/img/default.png" alt=""></p>
+              <h2 class="classic">NMS DeviouzOG</h2>
+              <ul class="list-inline">
+                <li><a href="https://twitter.com/Deviouz0G"><i class="fab fa-twitter fa-lg"></i></a></li>
+              </ul>
+              <h6>Content Manager US</h6>
+            </div>
 	        
 
 	    <div class="row justify-content-center">
